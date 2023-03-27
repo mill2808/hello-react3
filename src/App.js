6b7dc2d5e356de6ab9f6e567be02e7c3357cecd7
mill2808 <105799175+mill2808@users.noreply.github.com>
2023-03-27 12:26:12 -0400

    lecture notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+const magazines = [
+  {id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true},
+  {id: 1, title: 'Dwell', theme: 'architecture', isAvailable: true},
+  {id: 1, title: 'Communication Arts', theme: 'design', isAvailable: false},
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li 
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'red' : 'green'
+      }}
+      >
+        {zine.title}
+      </li>
+    );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img 
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+    </div>
+  );
+}
+
 
 function MagicButton() {
   return (
-    <button>Magic</button>
+    <div>
+      <h3>This is a magic button</h3>
+      <button>Magic</button>
+    </div>
   )
 }
 
@@ -11,21 +64,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Helen and MSU!
-        </a>
+        <ZineRack />
+        <Bookshelf />
+        <MagicButton />
       </header>
     </div>
   );
 }
+
 
 export default App;
