af7e772ed19e06e0c4f25792acefab23540ab010
mill2808 <105799175+mill2808@users.noreply.github.com>
2023-04-03 14:27:30 -0400

    fix css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { supabase } from './supabaseClient';
 import logo from './willow.jpg';
+import './App.css';
 
 function Library() {
   const [myBooks, setMyBooks] = useState([]);
